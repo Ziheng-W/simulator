e7520599ceb7376f20edbb3208070886dd21b00b
yibo <yibo@umich.edu>
2024-07-13 12:05:10 +0800

    update mapper.m

--- mapper.m
+++ mapper.m
@@ -4,7 +4,9 @@ function symbol_stream = mapper(tx_bitstream, modulation)
 % 00 --- 1/sqrt(2) + j * 1/sqrt(2)
 % 01 --- -1/sqrt(2) + j * 1/sqrt(2)
 % 11 --- -1/sqrt(2) - j * 1/sqrt(2)
-% 10 --- 1/sqrt(2) + j * 1/sqrt(2)
+% 10 --- 1/sqrt(2) - j * 1/sqrt(2)
+
+% You only need to implement "QPSK".
 
 % Input: tx_bitstream is a string, modulation is "QPSK"
 % Output: symbol_stream is a vector of symbols
